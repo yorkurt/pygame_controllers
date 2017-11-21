f05f97738523caa13ed177a90bfa82f0cd006428
Adam Silverman <32516544+ajs0429@users.noreply.github.com>
2017-11-21 18:42:08 -0500

    Created basic code to work with a PS4 controller

--- main.py
+++ main.py
@@ -10,19 +10,13 @@ class Main:
         self.screen = pygame.display.set_mode((self.SCREEN_WIDTH,self.SCREEN_HEIGHT))
 
         self.objects = []
-        self.mode = 1
-
-        pygame.font.init()
-        self.font = pygame.font.Font(pygame.font.get_default_font(),32)
 
     def setupGame(self):
         self.clock = pygame.time.Clock()
         self.FPS = 60
-        self.joy = joy.Joystick_L()
-        self.joy2 = joy.Joystick_R()
+        self.joy = joy.Joystick()
 
         self.objects.append(self.joy)
-        self.objects.append(self.joy2)
 
     def runGame(self):
         self.gameRunning = 1
@@ -30,80 +24,22 @@ class Main:
         while self.gameRunning:
             self.getInput()
             self.compute()
-            self.draw(self.screen)
+            self.draw()
             self.clock.tick(self.FPS)
-            
-            leftX = pygame.joystick.Joystick(0).get_axis(0)
-            leftY = -1 * pygame.joystick.Joystick(0).get_axis(1)
-            #rightX = pygame.joystick.Joystick(1).get_axis(0)
-            #rightY = -1 * pygame.joystick.Joystick(1).get_axis(1)
-
-            #handle buttons 
-            for event in pygame.event.get(pygame.JOYBUTTONUP): #event handling loop
-                #handle mode switching - buttons 8/9 on both sticks
-                print(event)
-                if (event.button == 7): #button 8 increases mode
-                    if (self.mode == 3):
-                        self.mode = 1
-                    else:
-                        self.mode = self.mode + 1
-                    print("Mode is now: " + str(self.mode))
-                if (event.button == 8): #button 9 decreases mode
-                    if (self.mode == 1):
-                        self.mode = 3
-                    else:
-                        self.mode = self.mode - 1
-                    print("Mode is now: " + str(self.mode))
-
 
     def getInput(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.gameRunning = 0
 
-        #if(self.joy.joyInitL == True):
-            pygame.display.set_caption(str(self.joy.x) + ', ' + str(self.joy.y) + ', ' + str(self.joy.rad) + ', ' + str(self.joy.throttle))
+        
+        pygame.display.set_caption(str(self.joy.x) + ', ' + str(self.joy.y) + ', ' + str(self.joy.rad))
 
-    def draw(self,surface):
+    def draw(self):
         self.screen.fill((255,255,255))
 
-        #for o in self.objects:
-        #    o.draw(self.screen)
-
-        
-        r = 200
-        w = surface.get_width()
-        h = surface.get_height()
-
-        for i in xrange(self.joy.numButtons):
-            if self.joy.buttons[i]:
-                col = (0,255,0)
-            else:
-                col = (64,0,64)
-            
-            text = self.font.render(str(i),1,col)
-            surface.blit(text,text.get_rect(centerx=w*(i+1)/(self.joy.numButtons+1),centery=h/2-30))
-
-        x = int(round(w/2+self.joy.x*r))
-        y = int(round(h/2+self.joy.y*r))
-        pygame.draw.aaline(surface,(128,128,128),(w/2,h/2),(x,y),1)
-        pygame.draw.circle(surface,(0,0,0),(x,y),8,4)
-        #pygame.draw.circle(surface,(0,255,255),(w/2,h/2),r,2)
-
-        for i in xrange(self.joy2.numButtons):
-            if self.joy2.buttons[i]:
-                col = (0,255,0)
-            else:
-                col = (64,0,64)
-            
-            text = self.font.render(str(i),1,col)
-            surface.blit(text,text.get_rect(centerx=w*(i+1)/(self.joy2.numButtons+1),centery=h/2+30))
-        
-        x1 = int(round(w/2+self.joy2.x*r))
-        y1 = int(round(h/2+self.joy2.y*r))
-        pygame.draw.aaline(surface,(128,0,0),(w/2,h/2),(x1,y1),1)
-        pygame.draw.circle(surface,(0,0,0),(x1,y1),8,4)
-        pygame.draw.circle(surface,(0,255,255),(w/2,h/2),r,2)
+        for o in self.objects:
+            o.draw(self.screen)
 
         pygame.display.flip()
 
