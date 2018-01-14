9cf9a67e15cf319efd504db20e8adeb7b41cb625
ajs0429 <ajs0429@my.yorku.ca>
2018-01-14 17:45:09 -0500

    Added button array

--- talker.py
+++ talker.py
@@ -2,20 +2,26 @@
 
 import rospy
 from std_msgs.msg import String#, Float64MultiArray
-from controller.msg import FloatList
+from controller.msg import FloatList, IntList
 #import main
 
 def talker():
     #float64[4] axes = [main.leftX,main.leftY,main.rightX,main.rightY]
-    pub = rospy.Publisher('controls', FloatList, queue_size=10)
+    pub_axes = rospy.Publisher('controls', FloatList, queue_size=10)
+    pub_buttons = rospy.Publisher('buttons', IntList, queue_size=10)
     rospy.init_node('controller_base', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     axes = FloatList()
+    buttons = IntList()
     while not rospy.is_shutdown():
         #axes.data = [main.leftX,main.leftY,main.rightX,main.rightY]
 	axes.data = [1,-1,0,1]
+	#buttons.data = main.buttonArr1
+	buttons.data = [1,0,1,0,1]
         rospy.loginfo(axes)
-        pub.publish(axes)
+        pub_axes.publish(axes)
+	rospy.loginfo(buttons)
+        pub_buttons.publish(buttons)
         rate.sleep()
 
 if __name__ == '__main__':
